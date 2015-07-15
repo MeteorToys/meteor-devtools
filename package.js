756b7fbd507e4abbe85133923600901e34cab954
Standard <standard@Savins-iMac.local>
2015-07-15 15:37:55 -0400

    Notes

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
 	name:    "meteortoys:allthings",
 	summary: "Insanely Handy Development Tools for Meteor",
-	version: "1.1.4",
+	version: "1.2.1",
 	git:     "https://github.com/MeteorToys/allthings.git",
 	documentation: "README.md",
 	debugOnly: true
 });
 
 Package.onUse(function(api) {
-	api.use("msavin:mongol@1.0.97"); 
-	api.use("msavin:jetsetter@1.0.17");
-	api.use("meteortoys:pro@0.3.0");
+	api.use("msavin:mongol@1.0.102");
+	api.use("msavin:jetsetter@1.0.19");
+	api.use("meteortoys:pro@0.6.0");
 });
